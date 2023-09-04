8349c0ae34e10e941242fc4809558175d4d56b28
RubenTheCoder <RubenBenjaminVos@gmail.com>
2023-09-04 14:50:34 +0200

    Began creating the topbar

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
 <!DOCTYPE html>
 <meta charset="UTF-8" />
+<html lang="en-us">
 
-<title>Travel the World | Shunji Railways</title>
+
+
+
+
+<head>
+    <!--[ Interface Metadata ]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--[ SEO Metadata ]-->
+    <title>Travel the World | Shunji Railways</title>
+</head>
+
+
+
+
+
+<body>
+    <!--[ Topbar ]-->
+    <header>
+        <button></button>
+        <h1>Shunji Railways</h1>
+    </header>
+</body>
+
+
+
+
+
+</html>
+<!-- Made by RubenTheCoder -->
