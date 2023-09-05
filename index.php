03892c87376180ac08b1cada04b103dcd7c63ee6
RubenTheCoder <RubenBenjaminVos@gmail.com>
2023-09-05 11:58:59 +0200

    Issue 5 - Added the CSS and connected it to index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
+<meta charset="UTF-8">
 <html lang="en-us">
 
 <head>
+    <link rel="stylesheet" href="default.css">
+
+    <!-- Sets the viewport scale to be the size of the screen -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
     <title>Travel the World | Shunji Railways</title>
 </head>
 
