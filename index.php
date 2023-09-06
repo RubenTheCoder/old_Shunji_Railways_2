73f711963cfd932e99a86a6e0bb9f4057ae8119d
RubenTheCoder <RubenBenjaminVos@gmail.com>
2023-09-06 11:34:57 +0200

    Issue 11 - Added a article and CSS for upcoming articles + reworked class names

--- index.php
+++ index.php
@@ -30,31 +30,86 @@
         <h1>Shunji Railways</h1>
     </header>
 
+
+
     <main>
         <!-- Welcome section -->
-        <img class="welcome-img" src="images/webpages/shunji_train_1.jpg" />
-        <h2 class="welcome-txt">Welcome to Shunji Railways</h2>
+        <aside class="welcome-banner">
+            <img src="images/webpages/shunji_train_1.jpg" />
+            <h2>Welcome to Shunji Railways</h2>
+            <p>Here you can find information for your next travel with us.</p>
+        </aside>
+
+        <!-- Safety Article -->
+        <article class="home-article">
+            <h3>Safety</h3>
+            <p>
+                At Shunji Railways, we put safety first.
+                This means we have some rules for our stations and trains
+            </p>
+            <h4>At the station</h4>
+            <ul>
+                <li>
+                    Don't board the train after the whistle has been blown.
+                    You could fall off the platform.
+                </li>
+                <li>Form a line and don't push people, they might fall on the rails.</li>
+                <li>
+                    Stay behind the yellow lines on open platforms.
+                    You could be hit or sucked onto the rails by a passing train.
+                </li>
+            </ul>
+            <h4>Inside the train</h4>
+            <ul>
+                <li>
+                    Don't leave luggage laying on the walking path.
+                    Someone might trip.
+                </li>
+                <li>
+                    Hold onto something when standing inside the train,
+                    or you might fall over.
+                </li>
+                <li>Don't open the doors until the train has fully stopped</li>
+                <li>
+                    If theres a emergency,
+                    contact our train staff using the emergency panels located next to the doors.
+                    In live threatening situations, call the emergency number (112).
+                </li>
+            </ul>
+        </article>
     </main>
+
+
 
     <footer class="default-footer">
         <!-- Contact info -->
         <h2>Contact info</h2>
         <ul>
-            <li><address>Tel: 01-5178321109</address></li>
-            <li><address>Email: shunjirailways@globanet.com</address></li>
+            <li>
+                <address>Tel: 01-5178321109</address>
+            </li>
+            <li>
+                <address>Email: shunjirailways@globanet.com</address>
+            </li>
         </ul>
 
         <!-- Address -->
         <h2>Address</h2>
         <ul>
-            <li><address>Street: Nichibotsu 5712, 7251CR</address>
-            <li><address>City: Globalpolis, Emeraldia</address>
+            <li>
+                <address>Street: Nichibotsu 5712, 7251CR</address>
+            <li>
+                <address>City: Globalpolis, Emeraldia</address>
         </ul>
 
         <!-- GitHub -->
         <h2>Website info</h2>
         <ul>
-            <li><a title="Open GitHub" href="https://github.com/RubenTheCoder/Shunji-Railways">Shunji Railways on GitHub</a>
+            <li>
+                <a title="Open GitHub" href="https://github.com/RubenTheCoder/Shunji-Railways">
+                    Shunji Railways on GitHub
+                </a>
+            </li>
         </ul>
     </footer>
 </body>
